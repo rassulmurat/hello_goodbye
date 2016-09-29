9f9142e06bfd1683e3a53da1f7a78ddcd060f7ef
rassulmurat <rassul1995@gmail.com>
2016-09-29 16:13:42 +0300

    finished sed and timeout

--- weather.sh
+++ weather.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 while true
 do
-	$(wget http://www.accuweather.com/en/kz/astana/222343/weather-forecast/222343 -O astana)
-	echo $(grep -E '<span class="local-temp">(.*)&deg;C</span>' astana)
-	echo end
-	sleep 5
+	wget http://www.accuweather.com/en/kz/astana/222343/weather-forecast/222343 -O astana 2> /dev/null
+	reg=$(grep -E '<span class="local-temp">(.*)&deg;C</span>' astana)
+	echo $(sed -r 's/.*>([0-9]*)&.*/\1/g' <<< "$reg")
+	timeout=$(sed -r 's/timeout=(.*)/\1/g' config.ini)
+	sleep "$timeout"
 done
